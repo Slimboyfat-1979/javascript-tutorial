419541493e8e6d45d6268ad91f7977bd8072aba1
Slimboyfat-1979 <slimboyfat1979@gmail.com>
2025-11-08 20:51:51 +1300

    Rendering two different slots

--- src/App.vue
+++ src/App.vue
@@ -23,24 +23,29 @@ const selectedMethod = computed(() => store.arrayMethod)
       class="text-center w-full p-5 text-6xl bg-linear-to-r from-purple-200 to-violet-400 text-white"
     >
       <div v-if="!selectedMethod">
-        <h2>Choose an Array Method To Begin</h2>
-        <div class="h-11 mt-2"></div>
-        <CardComponent></CardComponent>
+        <h1>Choose Your Array Method</h1>
+        <CardComponent #empty></CardComponent>
       </div>
       <div v-else>
-        <h2>{{ selectedMethod.name.slice(0, 1).toUpperCase() + selectedMethod.name.slice(1) }}</h2>
-        <p :key="selectedMethod.id" class="mt-2 text-2xl font-light italic">
-          Category: {{ selectedMethod.category }}
-        </p>
-
-        <CardComponent>
-          <InformationCard :methodInfo="selectedMethod"></InformationCard>
+        <h1>
+          Working With
+          {{ selectedMethod.name.slice(0, 1).toUpperCase() + selectedMethod.name.slice(1) }}
+        </h1>
+        <CardComponent #information>
+          <div class="flex justify-between align-middle h-[80%] text-purple-300">
+            <button class="cursor-pointer"><</button>
+            <button class="cursor-pointer">></button>
+          </div>
+          <div class="flex justify-center">
+            <div>.</div>
+            <div>.</div>
+            <div>.</div>
+          </div>
         </CardComponent>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
+<style scoped></style>
 
-</style>
--- src/UI/CardComponent.vue
+++ src/UI/CardComponent.vue
@@ -2,15 +2,7 @@
   <div
     class="bg-purple-100 border border-purple-300 rounded-lg shadow-md p-4 hover:shadow-lg transition duration-200 w-[80%] mt-7 mx-auto h-[50vh]"
   >
-    <slot></slot>
-    <div class="flex justify-between align-middle h-[80%] text-purple-300">
-      <button class="cursor-pointer"><</button>
-      <button class="cursor-pointer">></button>
-    </div>
-    <div class="flex justify-center">
-      <div>.</div>
-      <div>.</div>
-      <div>.</div>
-    </div>
+    <slot name="empty"></slot>
+    <slot name="information"></slot>
   </div>
 </template>
